f0d44298e0737422cfac3cfaea324808f3b3a4da
Konrad Gumowski <71015571+KonradGumowski@users.noreply.github.com>
2023-10-24 21:18:29 +0200

    Update Wektor2D.hpp _v2

--- Wektor2D.hpp
+++ Wektor2D.hpp
@@ -17,19 +17,20 @@ public:
     void setY(double a) { y = a; }
     double getX() { return x; }
     double getY() { return y; }
-friend Wektor2D operator+(Wektor2D A, Wektor2D B);
-friend Wektor2D operator*(Wektor2D A, Wektor2D B);
+//friend Wektor2D operator+(Wektor2D A, Wektor2D B);
+//friend Wektor2D operator*(Wektor2D A, Wektor2D B);
 private:
     double x;
     double y;
 };
 
 Wektor2D operator+(Wektor2D A, Wektor2D B) {
-//    Wektor2D C{ A.getX() + B.getX(), A.getY() + B.getY() };
-        Wektor2D C{ A.x + B.x, A.y + B.y };
+    Wektor2D C{ A.getX() + B.getX(), A.getY() + B.getY() };
+//        Wektor2D C{ A.x + B.x, A.y + B.y };
     return C;
 }
 Wektor2D operator*(Wektor2D A, Wektor2D B) {
-        Wektor2D C{ A.x * A.y+ B.x * B.y };
+        Wektor2D C{ A.getX() * A.getY()+ B.getX() * B.getY() };
+    //Wektor2D C{ A.x * A.y+ B.x * B.y };
     return C;
 }
