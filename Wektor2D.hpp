0ea180db5056b60796c4c3b5ea67aeba7e455c53
Konrad Gumowski <71015571+KonradGumowski@users.noreply.github.com>
2023-10-24 16:54:00 +0200

    Add files via upload

--- Wektor2D.hpp
+++ Wektor2D.hpp
@@ -1,4 +1,14 @@
+#include <iostream>
 class Wektor2D
 {
-    // Tutaj napisz implementacje klasy Wektor2D
+public:
+    Wektor2D() { }
+    int norm() {
+        return sqrt(x * x + y * y);
+    }
+    void print() {
+        std::cout << 333333 << std::endl;
+    }
+    int x;
+    int y;
 };
